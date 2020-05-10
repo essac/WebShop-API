608c4a859d80e0e913b846d1e3063d2fd4a95290
luxLX2 <luxlxdev@gmail.com>
2020-05-10 15:51:12 +0200

    Added some config codes to app.js and created config.js, Controllers and Routers folders.

---
+++ app.js
@@ -0,0 +1,19 @@
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.server = app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    user: 'ashour',
+    password: 'Ocikiq1989107',
+    server: 'myschoolcourse.database.windows.net',
+    database: 'Webshop'
+}
+
+module.exports = config;
